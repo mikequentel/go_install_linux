891752c1b21b959a81ef2237b25d9a4c4ddd51b5
Mike Quentel <mikequentel@users.noreply.github.com>
2019-04-23 17:53:43 -0400

    Include GOPATH and GOBIN in install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ curl -LO ${DOWNLOAD_URL_PREFIX}/${TAR_BALL}
 sudo tar -C /usr/local -xzf ${TAR_BALL}
 if [ -f ~/.bashrc ]; then
   cp ~/.bashrc ~/.bashrc.BAK
+  mkdir -p ~/go/bin
+  echo 'export GOPATH=~/go'>>~/.bashrc
+  echo 'export GOBIN=${GOPATH}/bin'>>~/.bashrc
   echo 'export PATH=$PATH:/usr/local/go/bin'>>~/.bashrc
 fi
 
