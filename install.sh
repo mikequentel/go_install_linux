16bec3ab0d71addb5ee1aa90003d202e6f71f9b3
Mike Quentel <mikequentel@users.noreply.github.com>
2019-04-23 17:49:31 -0400

    Update install.sh to use go1.12.4

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-curl -LO https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz
+DOWNLOAD_URL_PREFIX=https://dl.google.com/go
+TAR_BALL=go1.12.4.linux-amd64.tar.gz
+curl -LO ${DOWNLOAD_URL_PREFIX}/${TAR_BALL}
+sudo tar -C /usr/local -xzf ${TAR_BALL}
 if [ -f ~/.bashrc ]; then
   cp ~/.bashrc ~/.bashrc.BAK
   echo 'export PATH=$PATH:/usr/local/go/bin'>>~/.bashrc
