2650676f5291af191a5c387deaca3f5af017e4dd
Mike Quentel <mikequentel@users.noreply.github.com>
2020-01-27 17:30:16 -0500

    Update to go1.13.6.linux-amd64.tar.gz

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOWNLOAD_URL_PREFIX=https://dl.google.com/go
-TAR_BALL=go1.12.4.linux-amd64.tar.gz
+TAR_BALL=go1.13.6.linux-amd64.tar.gz
 curl -LO ${DOWNLOAD_URL_PREFIX}/${TAR_BALL}
 sudo tar -C /usr/local -xzf ${TAR_BALL}
 if [ -f ~/.bashrc ]; then
